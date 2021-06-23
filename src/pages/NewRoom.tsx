f43682b489f1998f466e79883ffa64828cb7fb60
VictorKengoo <victor.kengoo.kodama@accenture.com>
2021-06-23 17:48:05 -0300

    Dia 3:rocket:: Conexão banco Firebase

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,16 +1,37 @@
-import { useContext } from 'react'
+import { useState } from 'react'
+import { FormEvent } from 'react'
 import { Link } from 'react-router-dom'
-import { AuthContext } from '../contexts/AuthContext'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
 import { Button } from '../components/Button/Button'
+import { AuthContext } from '../contexts/AuthContext'
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
 
 import '../styles/auth.scss'
 
 export function NewRoom() {
-  // const { user } = useContext(AuthContext)
+  const { user } = useAuth()
+
+  const [newRoom, setNewRoom] = useState('')
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if (newRoom.trim() == '') {
+      return
+    }
+
+    const roomRef = database.ref('rooms')
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id
+    })
+
+  }
 
   return (
     <div id="page-auth" >
@@ -28,10 +49,12 @@ export function NewRoom() {
 
           <h2>Criar uma nova sala</h2>
 
-          <form>
+          <form onSubmit={handleCreateRoom}>
             <input
               type="text"
               placeholder="Nome da sala"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
             />
 
             <Button type='submit'>
