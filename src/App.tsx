904153fc5d531dbd0ee501c6c8871bafb4cb2fbf
VictorKengoo <victor.kengoo.kodama@accenture.com>
2021-06-23 04:04:23 -0300

    Dia 2:rocket:: newroom | Hooks, Context, Authentication | Routes | Firebase | Styles | Login

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path='/' exact component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
